ec146e69b3219e5e74a0b304cb125fd17bbf74e7
Andreea Damsa <damsa.nicoleta@yahoo.com>
2020-08-05 15:05:48 +0300

    0.1 First setup commit

---
+++ event-management-backend/src/test/java/ro/msg/event/management/eventmanagementbackend/EventManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.msg.event.management.eventmanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
