c253d518a664dbd8f96cbdcd40848c2270418bbe
Stefan Radu <Stefan.Radu@msg.group>
2020-08-11 13:31:57 +0300

    added security

--- event-management-backend/src/main/java/ro/msg/event/management/eventmanagementbackend/controller/EventController.java
+++ event-management-backend/src/main/java/ro/msg/event/management/eventmanagementbackend/controller/EventController.java
@@ -1,6 +1,7 @@
 package ro.msg.event.management.eventmanagementbackend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ public class EventController {
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public void deleteEvent(@PathVariable long id) {
         this.eventService.deleteEvent(id);
     }
