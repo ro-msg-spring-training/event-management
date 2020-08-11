1bc2acb34be7af10c2910e7d6be140a15e9b0df0
BudTheDude <bogdan_bud98@yahoo.com>
2020-08-11 13:43:50 +0300

    add event

--- event-management-backend/src/main/java/ro/msg/event/management/eventmanagementbackend/controller/EventController.java
+++ event-management-backend/src/main/java/ro/msg/event/management/eventmanagementbackend/controller/EventController.java
@@ -1,22 +1,52 @@
 package ro.msg.event.management.eventmanagementbackend.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ro.msg.event.management.eventmanagementbackend.embeddedid.EventSublocationID;
-import ro.msg.event.management.eventmanagementbackend.entity.Event;
-import ro.msg.event.management.eventmanagementbackend.entity.EventSublocation;
-import ro.msg.event.management.eventmanagementbackend.entity.Sublocation;
-import ro.msg.event.management.eventmanagementbackend.repository.EventRepository;
-import ro.msg.event.management.eventmanagementbackend.repository.EventSublocationRepository;
-import ro.msg.event.management.eventmanagementbackend.repository.SublocationRepository;
-import ro.msg.event.management.eventmanagementbackend.service.EventService;
+import ro.msg.event.management.eventmanagementbackend.dto.EventDTO;
+import ro.msg.event.management.eventmanagementbackend.entity.*;
+import ro.msg.event.management.eventmanagementbackend.service.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/events")
 public class EventController {
 
     private final EventService eventService;
+    private final PictureService pictureService;
+    private final SublocationService sublocationService;
+    private final EventSublocationService eventSublocationService;
+
+    @PostMapping
+    public EventDTO saveEvent(@RequestBody EventDTO eventDTO) throws Exception {
+
+
+        List<Long> sublocationIDs = eventDTO.getSubLocation();
+
+        ModelMapper modelMapper = new ModelMapper();
+        Event event = modelMapper.map(eventDTO, Event.class);
+
+        long eventID = eventService.saveEvent(event, sublocationIDs);
+
+        eventDTO.getPictureURL().forEach((url) -> pictureService.savePicture(new Picture(url, event)));
+
+        sublocationIDs.forEach((sublocationID) -> {
+
+            EventSublocationID esID = new EventSublocationID(eventID, sublocationID);
+            EventSublocation eventSublocation = new EventSublocation();
+            eventSublocation.setEventSublocationID(esID);
+            eventSublocation.setEvent(event);
+            eventSublocation.setSublocation(sublocationService.findById(sublocationID));
+            eventSublocationService.saveES(eventSublocation);
+        });
+
+        return eventDTO;
+    }
 
 }
+
--- event-management-backend/src/main/java/ro/msg/event/management/eventmanagementbackend/service/PictureService.java
+++ event-management-backend/src/main/java/ro/msg/event/management/eventmanagementbackend/service/PictureService.java
@@ -2,11 +2,16 @@ package ro.msg.event.management.eventmanagementbackend.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ro.msg.event.management.eventmanagementbackend.entity.Picture;
 import ro.msg.event.management.eventmanagementbackend.repository.PictureRepository;
 
 @Service
 @RequiredArgsConstructor
-public class PicureService {
+public class PictureService {
 
     private final PictureRepository pictureRepository;
+
+    public void savePicture(Picture picture){
+        pictureRepository.save(picture);
+    }
 }
