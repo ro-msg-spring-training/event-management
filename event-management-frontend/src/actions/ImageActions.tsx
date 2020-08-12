f7333eafed38934c404f7966717eaab258c1c5e7
Teofana Enachioiu <Teofana.Enachioiu@msg.group>
2020-08-12 11:28:14 +0300

    Preparing to introduce the S3 service

--- event-management-frontend/src/actions/ImageActions.tsx
+++ event-management-frontend/src/actions/ImageActions.tsx
@@ -1,10 +1,19 @@
 import { EventImage } from "../model/EventImage"
 
 export const UPDATE_EVENT_IMAGES = "UPDATE_EVENT_IMAGES"
+export const UPLOAD_EVENT_S3 = "UPLOAD_EVENT_S3"
 
 export const updateEventImages = (images: EventImage[]) => {
     return {
         type: UPDATE_EVENT_IMAGES,
+        payload: images
+    }
+}
+
+// to change the specification of the method
+export const uploadEventImagesS3 = (images: any) => {
+    return {
+        type: UPLOAD_EVENT_S3,
         payload: images
     }
 }
