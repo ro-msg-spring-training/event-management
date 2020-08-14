35a2a3168d28b6fa0261ff1018b9320311bb173b
BudTheDude <bogdan_bud98@yahoo.com>
2020-08-14 10:16:30 +0300

    almost final version

--- event-management-backend/src/main/java/ro/msg/event/management/eventmanagementbackend/repository/EventRepository.java
+++ event-management-backend/src/main/java/ro/msg/event/management/eventmanagementbackend/repository/EventRepository.java
@@ -7,16 +7,20 @@ import ro.msg.event.management.eventmanagementbackend.entity.Event;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 
 public interface EventRepository extends JpaRepository<Event, Long> {
 
     @Query("select e from Event e join EventSublocation es on e.id = es.event.id " +
-            "where (e.startDate>= :start and e.startDate<= :end) " +
-            "or (e.endDate<= :end and e.endDate>=:start)  and es.sublocation.id = :sublocation")
-    List<Event> findOverlappingEvents(@Param("start") LocalDate start,
-                                      @Param("end") LocalDate end,
+            "where (e.startDate>= :startDate and e.startDate<= :endDate and e.startHour >= :startHour and e.startHour <= :endHour) " +
+            "or (e.endDate <= :endDate and e.endDate >= :startDate and e.endHour <= :endHour and e.endHour >= :startHour)" +
+            " and es.sublocation.id = :sublocation")
+    List<Event> findOverlappingEvents(@Param("startDate") LocalDate startDate,
+                                      @Param("endDate") LocalDate endDate,
+                                      @Param("startHour") LocalTime startHour,
+                                      @Param("endHour") LocalTime endHour,
                                       @Param("sublocation") long sublocation);
 
 }
