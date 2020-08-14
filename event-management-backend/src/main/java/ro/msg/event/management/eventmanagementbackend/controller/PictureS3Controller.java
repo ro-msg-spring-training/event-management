8cefe720da37ad3f3f8c63d7b9e6045e78610e99
Stefan Radu <Stefan.Radu@msg.group>
2020-08-14 14:09:29 +0300

    changed PictureS3Controller to work on ec2 instance

--- event-management-backend/src/main/java/ro/msg/event/management/eventmanagementbackend/controller/PictureS3Controller.java
+++ event-management-backend/src/main/java/ro/msg/event/management/eventmanagementbackend/controller/PictureS3Controller.java
@@ -1,15 +1,10 @@
 package ro.msg.event.management.eventmanagementbackend.controller;
 
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.regions.Regions;
+import com.amazonaws.auth.InstanceProfileCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,38 +18,23 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/pictures")
-@PropertySource("classpath:aws.properties")
 @RequiredArgsConstructor
 public class PictureS3Controller {
 
     private final PictureS3Service pictureS3Service;
 
-    @Value("${accessKey}")
-    private String accessKey;
 
-    @Value("${secretKey}")
-    private String secretKey;
-
-    @Value("${bucketName}")
-    private String bucketName;
+    private String bucketName = "event-management-pictures";
 
     @PostMapping
     public List<URL> getPresignedUrl(@RequestBody PictureDTO pictureDTO) {
-
 
         List<String> objectKeys = pictureDTO.getPicturesToSave();
         List<String> picturesToDeleteUrls = pictureDTO.getPicturesToDelete();
 
 
-        AWSCredentials credentials = new BasicAWSCredentials(
-                accessKey,
-                secretKey
-        );
-
-        AmazonS3 s3Client = AmazonS3ClientBuilder
-                .standard()
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                .withRegion(Regions.EU_WEST_1)
+        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                .withCredentials(new InstanceProfileCredentialsProvider(false))
                 .build();
 
         pictureS3Service.deleteFromS3(picturesToDeleteUrls,bucketName,s3Client);
