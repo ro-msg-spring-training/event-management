93d7dbd4742d114e11049a98c887d60bce4413b1
Teofana Enachioiu <Teofana.Enachioiu@msg.group>
2020-08-18 13:33:01 +0300

    Changed style

--- event-management-frontend/src/components/eventCreateOrEdit/imagesSection/ImagesSectionDumb.tsx
+++ event-management-frontend/src/components/eventCreateOrEdit/imagesSection/ImagesSectionDumb.tsx
@@ -1,11 +1,11 @@
 import React, { useCallback, useState, useEffect } from 'react'
 import { useDropzone } from 'react-dropzone'
 import { ReactSortable } from 'react-sortablejs'
-import { Grid, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button, LinearProgress } from '@material-ui/core'
+import { Grid, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button, LinearProgress, Paper } from '@material-ui/core'
 import { useStyle } from '../../../styles/ImagesSectionStyles'
 import { EventImage } from '../../../model/EventImage'
 import CancelIcon from '@material-ui/icons/Cancel';
-import { useTranslation, Trans } from "react-i18next";
+import { useTranslation } from "react-i18next";
 
 
 interface ImagesSectionProps {
@@ -68,31 +68,45 @@ function ImagesSectionDumb({ isError, isLoading, eventImages, updateEventImages 
     }, [images, updateEventImages]);
 
     return (
-        <>
-
+        <Paper className={classes.imagesArea}>
             <div {...getRootProps()} className={classes.dragndrop}>
                 <input {...getInputProps()} />
                 <p>{t("welcome.imageDragAndDrop")}</p>
             </div>
 
-            {
-                isError ?
-                    <p>{t("welcome.imageErrorMessage")}</p> :
-                    isLoading ?
-                        <LinearProgress /> :
-                        images.length !== 0 ?
-                            <ReactSortable list={images} setList={setImages} direction="horizontal" animation={150} className={`${classes.imagesContainer} MuiGrid-root MuiGrid-container MuiGrid-spacing-xs-3`}>
-                                {images.filter(item => item.deleted === undefined).map(item => (
-                                    <Grid item xs={12} sm={6} md={4} lg={2} key={item.id} className={classes.imageWrapper}>
-                                        <CancelIcon
-                                            onClick={() => handleClickOpen(item)}
-                                            className={classes.deleteButton} />
-                                        <img alt={item.name} src={item.url} className={classes.image} />
-                                    </Grid>
-                                ))}
-                            </ReactSortable> :
-                            null
-            }
+            <div className={classes.imagesContainerWrapper}>
+                {
+                    isError ?
+                        <p>{t("welcome.imageErrorMessage")}</p> :
+                        isLoading ?
+                            <LinearProgress /> :
+                            images.length !== 0 ?
+                                <ReactSortable
+                                    list={images}
+                                    setList={setImages}
+                                    direction="horizontal"
+                                    animation={150}
+                                    className={`${classes.imagesContainer} MuiGrid-root MuiGrid-container MuiGrid-spacing-xs-3`}>
+                                    
+                                    {images.filter(item => item.deleted === undefined).map(item => (
+                                        <Grid
+                                            item
+                                            xs={12} sm={6} md={4} lg={2}
+                                            key={item.id}
+                                            className={classes.imageWrapper}>
+                                            
+                                            <CancelIcon
+                                                onClick={() => handleClickOpen(item)}
+                                                className={classes.deleteButton} />
+                                                
+                                            <img alt={item.name} src={item.url} className={classes.image} />
+                                        </Grid>
+                                    ))}
+
+                                </ReactSortable> :
+                                null
+                }
+            </div>
 
             <Dialog
                 open={open}
@@ -118,7 +132,7 @@ function ImagesSectionDumb({ isError, isLoading, eventImages, updateEventImages 
                     </Button>
                 </DialogActions>
             </Dialog>
-        </>
+        </Paper>
     )
 }
 
