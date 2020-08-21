60c444f7c987ba89e6ced5a8c114f22cfe468f49
Andreea Damsa <damsa.nicoleta@yahoo.com>
2020-08-21 10:57:09 +0300

    8.5 Finished implementation for this task(requires tests)

---
+++ event-management-backend/src/main/java/ro/msg/event/management/eventmanagementbackend/controller/TicketController.java
@@ -0,0 +1,29 @@
+package ro.msg.event.management.eventmanagementbackend.controller;
+
+import com.amazonaws.services.simpleworkflow.model.RespondActivityTaskCanceledRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ro.msg.event.management.eventmanagementbackend.entity.AvailableTicketsPerCategory;
+import ro.msg.event.management.eventmanagementbackend.service.TicketService;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/tickets")
+public class TicketController {
+    private final TicketService ticketService;
+
+    @GetMapping("/remaining/{id}")
+    public ResponseEntity<AvailableTicketsPerCategory> getAvailableTickets(@PathVariable Long id){
+        List<AvailableTicketsPerCategory> list = ticketService.getRemainingTickets(id);
+        return new ResponseEntity(list, HttpStatus.OK);
+    }
+
+}
