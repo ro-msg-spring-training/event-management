3f8dec6273a44b22c67943217baa28007acb295e
Dan Teodora-Alexandra <teodora_dan@yahoo.com>
2020-08-24 11:31:32 +0300

    Final version admin home page

--- event-management-frontend/src/sagas/rootSaga.tsx
+++ event-management-frontend/src/sagas/rootSaga.tsx
@@ -25,7 +25,7 @@ export default function* rootSaga() {
     watchFetchCustomEventsAsync(),
     watchFetchCustomHomeEventsAsync(),
 
-    watchFetchTicketsAsync()
+    watchFetchTicketsAsync(),
     
     loadEventWatcher(),
     deleteProductWatcher(),
