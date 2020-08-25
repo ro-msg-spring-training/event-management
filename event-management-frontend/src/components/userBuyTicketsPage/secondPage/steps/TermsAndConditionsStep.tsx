9abd97d394c9cb85558d482d6aa70644db3649c7
Bozdog Ioana <bozdogioana@yahoo.com>
2020-08-25 10:58:15 +0300

    Update Terms and Conditions

--- event-management-frontend/src/components/userBuyTicketsPage/secondPage/steps/TermsAndConditionsStep.tsx
+++ event-management-frontend/src/components/userBuyTicketsPage/secondPage/steps/TermsAndConditionsStep.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useStyles } from '../../../../styles/CommonStyles';
-import { Button, Grid, TextField, makeStyles } from '@material-ui/core';
+import { Button, Grid, TextField, makeStyles, Typography } from '@material-ui/core';
 import { userBuyTicketsStyle } from '../../../../styles/UserBuyTicketsStyle';
+import BuyTicketsPopup from '../BuyTicketsPopup';
 
 interface TermsAndConditionsStepProps {
   prevStep: () => void,
 }
 
 function TermsAndConditionsStep({ prevStep }: TermsAndConditionsStepProps) {
+  const [open, setOpen] = useState(false);
   const buttonClass = useStyles();
   const classes = userBuyTicketsStyle();
 
+  let handleEventBuyTickets = (): void => {
+    setOpen(true);
+  }
+
   return (
     <>
-      <h1>Terms Step (4)</h1>
+      <Typography className={classes.typography}>Terms and Conditions</Typography>
+      <Grid item container direction="row" justify="center" alignItems="center">
+        <Button variant="contained" className={`${buttonClass.buttonStyle2} ${buttonClass.buttonStyle3}`} onClick={handleEventBuyTickets}> BUY TICKETS </Button>
 
-      <Grid item container direction="row" justify="center" alignItems="center" className={classes.button}>
-        <Grid item xs={4} sm={3} md={2} lg={1} xl={1}>
-          <Button variant="contained" className={`${buttonClass.buttonStyle2} ${buttonClass.buttonStyle3}`} onClick={prevStep}> PREV </Button>
+        <Grid item container direction="row" justify="center" alignItems="center" className={classes.button}>
+          <Grid item xs={4} sm={3} md={2} lg={1} xl={1}>
+            <Button variant="contained" className={`${buttonClass.buttonStyle2} ${buttonClass.buttonStyle3}`} onClick={prevStep}> PREV </Button>
+          </Grid>
+          <Grid item xs={4} sm={3} md={2} lg={1} xl={1} />
         </Grid>
-        <Grid item xs={4} sm={3} md={2} lg={1} xl={1} />
       </Grid>
+
+      <BuyTicketsPopup
+        open={open}
+        setOpen={setOpen}
+      />
     </>
   );
 };
