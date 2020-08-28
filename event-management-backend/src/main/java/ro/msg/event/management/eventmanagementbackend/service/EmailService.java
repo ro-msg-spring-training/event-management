f7bb03d7c432fa4b62ed010e035f936dbb2cdbee
Andreea Damsa <damsa.nicoleta@yahoo.com>
2020-08-28 10:33:50 +0300

    13.1 Small changes

--- event-management-backend/src/main/java/ro/msg/event/management/eventmanagementbackend/service/EmailService.java
+++ event-management-backend/src/main/java/ro/msg/event/management/eventmanagementbackend/service/EmailService.java
@@ -3,7 +3,6 @@ package ro.msg.event.management.eventmanagementbackend.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
