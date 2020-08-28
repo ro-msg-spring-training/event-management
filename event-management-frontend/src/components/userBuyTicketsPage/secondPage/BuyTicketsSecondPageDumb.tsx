91668e1343de2af4682c86d40dd12a2987f9cb14
Bozdog Ioana <bozdogioana@yahoo.com>
2020-08-28 18:57:05 +0300

    Change structure

--- event-management-frontend/src/components/userBuyTicketsPage/secondPage/BuyTicketsSecondPageDumb.tsx
+++ event-management-frontend/src/components/userBuyTicketsPage/secondPage/BuyTicketsSecondPageDumb.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import EmailStep from './steps/EmailStep';
 import Booking from '../../../model/Booking';
 import { Tooltip, IconButton } from '@material-ui/core';
@@ -15,84 +15,29 @@ interface BuyTicketsSecondPageDumbProps {
   gotoFirstPage: () => void,
   gotoEventListPage: () => void,
   ticketCategories: TicketAvailabilityData[],
-  eventId: number | string,
   booking: Booking,
   setBooking: (booking: Booking) => void,
   addBookings: (booking: Booking) => void,
+  step: number,
+  prevStep: () => void,
+  nextStep: () => void,
+  handleEnterKey: (e: any) => void,
+  handleTicketsStepChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
+  ticketAmount: (TicketsPerCateory[]),
+  handleEmailStepChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
+  handleNameStepChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
+  ticketNames: TicketNames[],
+  setTicketNames: (ticketNames: TicketNames[]) => void,
+  checked: boolean,
+  handleCheckboxChange: (event: React.ChangeEvent<HTMLInputElement>) => void,
+  setChecked: (checkedState: boolean) => void,
 }
 
-function BuyTicketsSecondPageDumb({ gotoFirstPage, gotoEventListPage, ticketCategories, eventId, booking, setBooking, addBookings }: BuyTicketsSecondPageDumbProps) {
-  const [step, setStep] = useState(1);
-  const [ticketAmount, setTicketAmount] = useState<TicketsPerCateory[]>([]);
-  const [checked, setChecked] = useState(false);
-  //ticketNames has this structure so wehn the user modifies a name text field I know where to apply the change 
-  const [ticketNames, setTicketNames] = useState<TicketNames[]>([]);
-
-  let today = new Date(new Date().toString().split('GMT')[0] + ' UTC').toISOString().split('.')[0]
-
-  let initialTicketState: TicketsPerCateory[] = [];
-  useEffect(() => {
-    ticketCategories.map((ticket) => initialTicketState.push({ category: ticket.title, quantity: 0 }))
-    setTicketAmount(initialTicketState);
-
-    let oldBooking = { ...booking };
-    oldBooking.eventId = Number(eventId);
-    oldBooking.bookingDate = today;
-    setBooking(oldBooking);
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-
-  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setChecked(event.target.checked);
-  };
-
+function BuyTicketsSecondPageDumb({ gotoFirstPage, gotoEventListPage, ticketCategories, booking, setBooking, addBookings,
+  step, prevStep, nextStep, handleEnterKey, handleTicketsStepChange, ticketAmount,
+  handleEmailStepChange, handleNameStepChange, ticketNames, setTicketNames, checked,
+  handleCheckboxChange, setChecked }: BuyTicketsSecondPageDumbProps) {
   const classes = BuyTicketsSecondPageStyle();
-
-  // Proceed to next step
-  const nextStep = () => { setStep(step + 1); };
-
-  // Go back to prev step
-  const prevStep = () => { setStep(step - 1); };
-
-  const handleEnterKey = (e: any): void => { e.keyCode === 13 && e.preventDefault(); }
-
-  const handleTicketsStepChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    const { name, value } = e.target;
-    const index = ticketCategories.findIndex(ticket => ticket.title === name)
-    ticketCategories[index].remaining >= Number(value) ?
-      setTicketAmount(ticketAmount.map(item => (item.category === name ? { ...item, 'quantity': Number(value) } : item))) :
-      console.log("Error not that many tickets in stock");
-  }
-
-  const handleEmailStepChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    const { name, value } = e.target;
-
-    let newBooking = { ...booking };
-    newBooking.email = value;
-    setBooking(newBooking);
-  }
-
-  const handleNameStepChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    const { name, value } = e.target;
-
-    //VIP_0 => ticketData[0] = VIP; ticketData[1] = 0
-    let ticketData = name.split("_");
-
-    //find the ticket category and its names array
-    let ticketToUpdate = ticketNames.find(ticket => (ticket.ticketTitle === ticketData[0]));
-
-    //set the new value to the specified position in the names array
-    ticketToUpdate!.names[Number(ticketData[1])] = value;
-
-    let ticketNamesCopy = [...ticketNames];
-    let index = ticketNames.findIndex(ticket => (ticket.ticketTitle === ticketData[0]))
-    let replacedTicket = { ...ticketNamesCopy[index] }
-    replacedTicket = ticketToUpdate as TicketNames;
-    ticketNamesCopy[index] = replacedTicket;
-    setTicketNames(ticketNamesCopy);
-  }
 
   const buttons =
     <>
