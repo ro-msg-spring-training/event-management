2a97ecbac501d541014101c7d58867f4fedd3d45
Julia Iakab <Julia.Iakab@msg.group>
2020-09-01 09:30:16 +0300

    Add styling to >remove ticket category< button

--- event-management-frontend/src/styles/CategoryCardStyle.tsx
+++ event-management-frontend/src/styles/CategoryCardStyle.tsx
@@ -52,7 +52,12 @@ export const useStylesCategoryCard = makeStyles((theme: Theme) => ({
     textAlign: "center",
     color: theme.palette.secondary.main,
     textTransform: "uppercase",
-    //background: "linear-gradient(90deg, #f9c929 20%, #f2ac0a 90%)",
+    backgroundSize: "200%",
+    transition: "0.3s",
+    "&:hover": {
+      backgroundPosition: "right",
+    },
+    backgroundImage: "linear-gradient(45deg, #f9c929 10%, #f2ac0a 50%, #ed4d6e 90%)",
     borderRadius: "20px",
     height: "35px",
     marginTop: theme.spacing(2),
