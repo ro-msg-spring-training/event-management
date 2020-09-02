ef33b63beb1e759d53e16b2440da964efbc77f21
Bozdog Ioana <bozdogioana@yahoo.com>
2020-09-03 02:50:35 +0300

    Small changes

--- event-management-frontend/src/components/userEventDetailsPage/CarouselSlide.tsx
+++ event-management-frontend/src/components/userEventDetailsPage/CarouselSlide.tsx
@@ -3,12 +3,11 @@ import { Grid, CardMedia } from '@material-ui/core';
 import Arrow from './Arrow';
 import { EventImage } from '../../model/EventImage';
 import { carouselSlideStyle } from '../../styles/CarouselSlideStyles';
+import { testImages } from '../../utils/CarouselSlideUtils';
 
 interface Props {
   images: EventImage[];
 }
-
-const testImages = [{ url: 'https://i.ibb.co/KNwnXRj/no-image.jpg' }];
 
 function CarouselSlide({ images }: Props) {
   const [index, setIndex] = useState(0);
